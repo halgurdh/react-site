c7e184da1a987468ce98e21884eb18ccd0e6824f
Halgurd Hussein <halgurd.hussein@heroes.nl>
2022-08-02 11:49:30 +0200

    change back to travel

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,40 +5,40 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Start taking CONTROL over your finances!</h1>
+      <h1>Check out these EPIC Destinations!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
               src='images/img-9.jpg'
-              text='Know how much you spend/earn'
-              label='Secure'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
               path='/services'
             />
             <CardItem
               src='images/img-2.jpg'
-              text='Minimalistic view of budget software'
-              label='Minimalist'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
               src='images/img-3.jpg'
-              text='Bring your own calculator!'
-              label='Calculated'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
               path='/services'
             />
             <CardItem
               src='images/img-4.jpg'
-              text='Get help from someone in our team!'
-              label='FinanceBuddy'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
               path='/products'
             />
             <CardItem
               src='images/img-8.jpg'
-              text='Keep track of your NetWorth every month!'
-              label='Growth'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
               path='/sign-up'
             />
           </ul>
